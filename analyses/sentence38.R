8bc37872fb62bbdadbf3b779a17106fa921842af
Michael Cysouw <cysouw@mac.com>
2018-06-13 13:49:25 +0200

    adding 38

--- analyses/sentence38.R
+++ analyses/sentence38.R
@@ -12,28 +12,26 @@ source("preamble.R")
 
 #' Word order
 
-namesOrder <- "X1 X2 Die1 Leute2 heute1 es alles sind heute2 die2 Leute2"
+namesOrder <- "alle die leute heute X sind heute die leute X alle X leute heute draussen aufm feld draussen und tun zu mähen aufm feld draussen"
 
-dataOrder <- getAlignSyntax("../data/WS38/WS38_1syntax.txt", 7)
+dataOrder <- getAlignSyntax("../data/WS38/WS38_1syntax.txt", 7, names = namesOrder)
 
 # make into syntactically interesting features by using recoding profile
-# write.recoding(dataOrder, file = "../data/WS38/tmp.yml", attr = list( c(3,7,12), 5, c(9,10), 11, c(15,25), c(16,17), c(24,21,23,26,27), c(21,22,23,26,27), c(14,15,18,20,21,23,25:28)))
+# write.recoding(dataOrder, file = "../data/WS38/tmp.yml", attr = list( c(3, 4), c(1,3,9,11,13) ,c(7,11,14), c(15, 18, 25), c(19, 21), 20, c(17, 24)))
 order <- recode(dataOrder, "../data/WS38/syntaxRecoding.yml")
 
 # plot ordering parameters	
 
 titlesOrder <- list(
-	  tu.hinein = "Tu HINEIN"
-	, die.kohle = "Article before \'Kohle\'"
-	, den.ofen = "Article before \'Ofen\'"
-	, alternative.verb = "Extra verb like \'tu Kohle EINLEGEN"
-	, presence.etwas = "ETWAS Kohle"
-	, die.milch = "Article before \'Milch\'"
-	, position.kochen = "Position \'kochen\'"
-	, absence.zu = "anfängt ZU kochen"
-	, anfangen = "ANFÄNGT zu kochen"
-	, order.verbs = "Order Auxiliary verb and \'kochen\'"
-)  
+	  Vorfeld = "Vorfeldbesetzung"
+	, Mittelfeld = "Mittelfeldbesetzung"
+	, alle.Leute = "Position \'alle\'"
+	, draussen = "Position \'draussen\'"
+	, zu.mähen = "Infinitiv"
+	, tun.mähen = "Hilfsverb"
+	, feld = "Position \'Feld\'"
+)
+
 
 sapply(seq_along(titlesOrder), function(x) {
 	plotSingle(order[,names(titlesOrder)[x]], titlesOrder[[x]])
